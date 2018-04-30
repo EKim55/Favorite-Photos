191226664df3a47adb476ef3931dd46fa3c01c49
CSSE Department <csse@cssemm-7.wlan.rose-hulman.edu>
2018-04-30 12:11:37 -0400

    Added gallery support

--- Favorite Photos/FavoritePhotoViewController.swift
+++ Favorite Photos/FavoritePhotoViewController.swift
@@ -20,6 +20,31 @@ class FavoritePhotoViewController: UIViewController {
     }
     
     @IBAction func takePhoto(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        present(imagePicker, animated: true)
+    }
+}
+
+// MARK: UIImagePicker controller delegate methods
+
+extension FavoritePhotoViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imageView.image = image
+        }
+        picker.dismiss(animated: true)
+    }
 }
